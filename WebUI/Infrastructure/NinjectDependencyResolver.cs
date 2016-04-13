47aca66ba264c0e489f8830c5a77eed8e5133fee
dimahavrylevych <dimahavrylevych@gmail.com>
2016-04-13 10:14:57 +0000

    Creted a linked view, that displays basic list of books

---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book { Name = "Book A", Author = "Author A", Price = 1111},
+                new Book { Name = "Book B", Author = "Author B", Price = 2222},
+                new Book { Name = "Book C", Author = "Author C", Price = 111221}
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+
+}
